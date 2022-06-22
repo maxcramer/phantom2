9e9908b92685ad8fcb71492f096175404416d81d
maxcramer <maximiliancramer@yahoo.com>
2022-06-22 14:37:58 +0100

    urls being added properly to localHost

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import UrlInput from "./components/UrlInput/UrlInput";
 
-import UrlList from "./components/urlList/UrlList";
+import UrlList from "./components/UrlList/UrlList";
 
 function App() {
   return (
     <div className="App">
+      <UrlInput />
       <UrlList />
     </div>
   );
---
+++ src/components/UrlInput/UrlInput.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function UrlInput() {
+  const [url, setUrl] = useState("");
+  const [urls, setUrls] = useState([]);
+
+  const addUrl = (e) => {
+    if (url) {
+      const newUrl = {
+        id: new Date().getTime().toString(),
+        title: url,
+      };
+      setUrls([...urls, newUrl]);
+      localStorage.setItem("localUrls", JSON.stringify([...urls, newUrl]));
+      setUrl("");
+    }
+  };
+  return (
+    <div>
+      <h1>Url Checker</h1>
+      <div>
+        <input
+          name="url"
+          type="url"
+          placeholder="enter url"
+          className="form-control"
+          onChange={(e) => setUrl(e.target.value)}
+        />
+      </div>
+      <div>
+        <button onClick={addUrl}>Submit</button>
+      </div>
+    </div>
+  );
+}
+
+export default UrlInput;
